80d5b08243fb5dd3c4ab97cf91e70cd658a973cd
mrvirgozki <mrvirgozki@gmail.com>
2026-09-17 19:46:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,14 @@ PORT="${PORT:-8080}"
 echo "✅ Napiling Proxy: $PROXY_ENGINE"
 echo "✅ Port: $PORT"
 
-# Simulan Xray
+# Simulan Xray (mabilis lang)
 /usr/local/bin/xray run -c /etc/xray.json > /var/log/xray.log 2>&1 &
-sleep 3
+sleep 1
 
 # Simulan ang napiling proxy
 case "$PROXY_ENGINE" in
   openresty)
+    /usr/local/openresty/nginx/sbin/nginx -t || exit 1
     exec /usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
     ;;
   envoy)
@@ -25,6 +26,7 @@ case "$PROXY_ENGINE" in
     exec /usr/bin/caddy run --config /etc/Caddyfile --listen 0.0.0.0:$PORT
     ;;
   *)
+    /usr/local/openresty/nginx/sbin/nginx -t || exit 1
     exec /usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
     ;;
 esac
